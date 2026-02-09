e76ee0b341b8914e143b69ced9cfb589eac4553f
joecipale <72520104+jcipale@users.noreply.github.com>
2026-02-08 17:33:09 -0800

    Delete sql/Audit_Recordings.sql

--- sql/Audit_Recordings.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE audit_recordings (
-    audit_id INT AUTO_INCREMENT PRIMARY KEY,
-    recording_id INT,
-    action ENUM('INSERT','UPDATE','DELETE'),
-    action_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    user_name VARCHAR(100),
-    notes TEXT
-);
-
